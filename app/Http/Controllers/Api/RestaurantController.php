f6dda4a6940d39411945eea110f652fe3a3431ef
lamiaa <lamiaa.elmansy@appssquare.com>
2023-10-04 11:04:16 +0300

    sales module

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -118,7 +118,6 @@ class RestaurantController extends Controller
         $skip = $request->skip ? $request->skip : 0;
         $take = $request->take ? $request->take : 10;
 
-
         $resturant = Resturant::findOrFail($resturant_id);
         $reviews = $resturant->reviews->skip($skip)->take($take);
         $count = $resturant->reviews->count();
